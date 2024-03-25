858b7af0315a5d1effbfbf7f00eadc80586b0d1b
ShrewdLuni <ander113q@gmail.com>
2024-03-25 23:14:49 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  
+  var text : string = `$$$$$$$$$$\n$$$$$$$$$$\n$$$$$$$$$$\n$$$$$$$$$$`
+
+
+
+  return (
+    <div className="grid items-center justify-center h-screen bg-black">
+      <pre className="text-white">{text}</pre>
+    </div>
+  )
+}
+
+export default App
