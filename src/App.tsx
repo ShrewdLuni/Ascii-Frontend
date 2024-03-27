1146f7cef9a77e3da4af3b0b6db1a2b317601966
ShrewdLuni <ander113q@gmail.com>
2024-03-27 23:35:05 +0200

    feat: add image settings template

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { cn } from "./lib/utils"
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Slider } from "./components/ui/slider"
 
 function App() {
 
@@ -17,13 +19,64 @@ function App() {
   ]
   
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black">
-      <p className="m-0 leading-[0]">
-        {text.map(item => {
-            return  <span className="text-[8px] leading-none m-0" style={{color: `rgb(${item.Color.R},${item.Color.G},${item.Color.B})`}}>{item.Ascii} {item.Ascii == "\n" && <br/>}</span>
-          }
-        )}
-      </p>
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <div className="border-solid border-8 border-black p-10">
+        <p className="m-0 leading-[0]">
+          {text.map(item => {
+              return  <span className="text-[50px] leading-none m-0" style={{color: `rgb(${item.Color.R},${item.Color.G},${item.Color.B})`}}>{item.Ascii} {item.Ascii == "\n" && <br/>}</span>
+            }
+          )}
+        </p>
+      </div>
+      <div className="mt-[10vh] w-screen">
+        <div className="flex flex-row gap-x-32 justify-center">
+          <div className="flex flex-col gap-y-10 w-[30vh]">
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-10 w-[30vh]">
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+            <div className="flex flex-row gap-x-8">
+              <p>Text</p>
+              <Slider defaultValue={[50]} max={100} step={1} />
+              <Input type="number" className="w-[25%]"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
