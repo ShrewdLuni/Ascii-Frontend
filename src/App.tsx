1a1fecf4c61297f27d767c9034a0ed70875e88f8
ShrewdLuni <ander113q@gmail.com>
2024-04-02 17:46:29 +0300

    feat: add responsive display

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import { useState } from "react"
 import { Display } from "./components/Display"
-import { Settings } from "./components/Settings"
-import axios from "axios"
 import { Input } from "./components/ui/input"
+
+import axios from "axios"
 
 function App() {
 
-  let old = [
-  {Ascii : "$", Color:{R:0,G:255, B:0}},]
 
 
-  const [text,setText] = useState<{Ascii: string;Color: {R: number;G: number;B: number;};}[]>()
+  const [displayData,setDisplayData] = useState<{Ascii: string;Color: {R: number;G: number;B: number;};}[]>()
+  const sizes = {xs: 1, sm: 2, md: 4, lg: 8, xl: 16}
+
 
   function handleApi(event : React.ChangeEvent<HTMLInputElement>) {
     const formData = new FormData()
-    let image = event.target!.files![0];
-    formData.append("image", image)
-    console.log(formData)
-    axios.post("http://localhost:3001/test",formData).then((res) => setText(res["data"]))
+    formData.append("image", event.target!.files![0])
+    axios.post("http://localhost:3001/test",formData).then((res) => setDisplayData(res["data"]))
   }
 
   return (
-    <div className="flex bg-black h-screen w-screen items-center justify-center overflow-hidden">
-      <div className="flex flex-col justify-center">
-        <Display data={text == undefined ? old : text}/>
+    <div className="flex bg-black h-screen w-screen items-center justify-center">
+      <div className="flex flex-col">
+        <Display displayData={displayData} renderSize={sizes["md"]}/>
         <Input className="" type="file" name="file" onChange={(e) => handleApi(e)}/>
       </div>
     </div>
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,16 +1,17 @@
-
 interface DisplayProps {
-  data : {Ascii : string, Color : {R : number,G : number, B : number}}[] | undefined
+  displayData: {Ascii : string, Color : {R : number,G : number, B : number}}[] | undefined
+  renderSize: number
 }
 
-export const Display = ({data} : DisplayProps) => {
-  let size = 1;
+export const Display = ({displayData,renderSize} : DisplayProps) => {
 
-  return data != undefined && (
-    <p className={`text-[${size}px] leading-[${size*1.5}px]`}>
-      {data.map((item,i) => {
-        return  <span key={i} className="font-mono font-bold" style={{color: `rgb(${item.Color.R},${item.Color.G},${item.Color.B})`} }>{item.Ascii != "enter" ? item.Ascii : ""} {item.Ascii == "enter" && <br/>}</span>
-      })}
-    </p>
+  return displayData != undefined && (
+    <div className="overflow-scroll no-scrollbar max-h-[80vh]">
+      <p className={`text-[${renderSize}px] leading-[${renderSize * 1.5}px] font-mono font-bold`}>
+        {displayData.map((item,i) => {
+          return  <span key={i} style={{color: `rgb(${item.Color.R},${item.Color.G},${item.Color.B})`} }>{item.Ascii != "enter" ? item.Ascii : ""} {item.Ascii == "enter" && <br/>}</span>
+        })}
+      </p>
+    </div>
   )
 }
