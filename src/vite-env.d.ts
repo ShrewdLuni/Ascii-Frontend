0f9672414d949fd825e08042c743574fae5887db
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-02-22 00:57:12 +0200

    fix: right env(i hope)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_YOUR_URL: string;
   readonly VITE_REALM: string;
   readonly VITE_CLIENT_ID: string;
- 
+  SERVER_URL: string; 
 }
 
 interface ImportMeta {
